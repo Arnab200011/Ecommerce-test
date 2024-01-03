d54b8eada8475b006dbba0bb442c31a59fff91d7
Arnab200011 <arnabmondal959@gmail.com>
2024-01-03 19:18:38 +0530

    v1-testing

---
+++ script.js
@@ -0,0 +1,68 @@
+
+
+
+
+let profile = true;
+document.getElementById("profile-div").addEventListener("click", () => {
+  if (profile) {
+    document.querySelector(".profile-info-div").style.display = "block";
+    profile = false;
+  } else {
+    document.querySelector(".profile-info-div").style.display = "none";
+    profile = true;
+  }
+});
+
+
+
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+
+let counter = 1;
+setInterval(function () {
+  document.querySelector("#radio" + counter).checked = true;
+  counter += 1;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 4000);
+
